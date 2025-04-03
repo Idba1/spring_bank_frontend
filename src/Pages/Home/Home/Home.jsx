b6cb8bc1c90e59ddfbf4a59bd23f58e4edb02940
Idba1 <moniraislamself@gmail.com>
2025-04-03 15:55:02 +0600

    add Enhance Your Wealth Safely section slider

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from '../Banner/Banner'
 import Needs from '../Needs/Needs'
+import Slider from '../Slider/Slider'
 
 const Home = () => {
     return (
         <div className='container mx-auto'>
             <Banner />
             <Needs />
+            <Slider />
         </div>
     )
 }
